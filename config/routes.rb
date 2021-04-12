638401d79ec0c36ffadcb334874abb0f1d5df15c
justpiyoosh <justpiyoosh@gmail.com>
2021-04-12 21:05:09 +0530

    Final Commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
