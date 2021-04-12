44d135260fe523f7783beee14e959c657c418eb3
justpiyoosh <justpiyoosh@gmail.com>
2021-04-12 22:12:32 +0530

    Create a new model User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_122046) do
+ActiveRecord::Schema.define(version: 2021_04_12_161542) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2021_04_12_122046) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.text "occupation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
